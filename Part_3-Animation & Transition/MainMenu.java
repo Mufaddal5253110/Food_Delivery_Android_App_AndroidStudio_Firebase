af1a447ba3eca54695a04f0fa142129f6e37e838
MUFADDAL SHAKIR <60619133+Mufaddal5253110@users.noreply.github.com>
2020-08-01 10:23:39 +0530

    Part-3

---
+++ Part_3-Animation & Transition/MainMenu.java
@@ -0,0 +1,106 @@
+package my.foodon.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainMenu extends AppCompatActivity {
+
+    Button signinemail,signinphone,signup;
+    ImageView bgimage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        final Animation zoomin = AnimationUtils.loadAnimation(this,R.anim.zoomin);
+        final Animation zoomout = AnimationUtils.loadAnimation(this,R.anim.zoomout);
+
+        bgimage=findViewById(R.id.back2);
+        bgimage.setAnimation(zoomin);
+        bgimage.setAnimation(zoomout);
+
+        zoomout.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                bgimage.startAnimation(zoomin);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        zoomin.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                bgimage.startAnimation(zoomout);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        signinemail=(Button)findViewById(R.id.SignwithEmail);
+        signinphone=(Button)findViewById(R.id.SignwithPhone);
+        signup=(Button)findViewById(R.id.Signup);
+
+        signinemail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signemail = new Intent(MainMenu.this,ChooseOne.class);
+                signemail.putExtra("Home","Email");
+                startActivity(signemail);
+                finish();
+            }
+        });
+
+        signinphone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signphone = new Intent(MainMenu.this,ChooseOne.class);
+                signphone.putExtra("Home","Phone");
+                startActivity(signphone);
+                finish();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signup = new Intent(MainMenu.this,ChooseOne.class);
+                signup.putExtra("Home","Signup");
+                startActivity(signup);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.gc();
+    }
+}
